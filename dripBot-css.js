7fbf00b8bbaa67882e169ed50962b4c2c471199b
Andrew Potter <apottere@gmail.com>
2014-02-07 15:00:20 -0500

    Update with clearfix.

--- dripBot-css.js
+++ dripBot-css.js
@@ -1,3 +1,12 @@
 $('#dripbot-title').css({
 	"text-align": "left"
+	"display": "inline-block",
+	"float": "left",
+	"margin-right": "20px"
+});
+
+$('#dripbot-title').after('').css({
+        "display": "block",
+        "height": "0",
+        "clear": "both"
 });
