b302f228324aa727ebdb5807a03098d1059369cc
Andrew Potter <apottere@gmail.com>
2014-02-11 14:33:45 -0500

    Add argument to leaderboard script.

--- leaderboard.sh
+++ leaderboard.sh
@@ -5,12 +5,20 @@ getNextName() {
 	curl -s -X POST https://apmgui.dripstat.com/rest/l3/game/lb?name=$name | jsawk 'lb = this.lb; next = null; if(lb.length > 2) { next = lb[1]; } else { next = lb[0]; }; return next.name + "&" + next.score + "&" + next.rank'
 }
 
+getThisName() {
+	local name=$(sed -e 's/ /%20/g' <<< "$1")
+	curl -s -X POST https://apmgui.dripstat.com/rest/l3/game/lb?name=$name | jsawk 'lb = this.lb; next = null; if(lb.length > 2) { next = lb[2]; } else { next = lb[1]; }; return next.name + "&" + next.score + "&" + next.rank'
+}
+
 main() {
-	local reference='bottom'
+	IFS='&'
+	local reference=${1:-'bottom'}
 	local line aline name score rank
 	name=$reference
+	line=$(getThisName "$name")
+	echo $line
+	list="$list\n"$(tr '&' ' ' <<< "$line")
 
-	IFS='&'
 	while [[ $rank -ne 1 ]]; do
 
 		line=$(getNextName "$name")
