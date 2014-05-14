ac8c2c66bb76b55711f856392235c183ba64cfaf
Andrew Potter <apottere@gmail.com>
2014-05-14 11:04:16 -0400

    Remove concurrent modification.

--- dripBot.js
+++ dripBot.js
@@ -1023,15 +1023,18 @@ $dripBot = (function($, oldDripBot, isPro) {
 
 	var purge = function() {
 		stop();
-		setTimeout(function() {
-			for ( var i = 0, len = localStorage.length; i < len; ++i ) {
-				if(/^dsb\./.test(localStorage.key(i))) {
-					try {
-						localStorage.removeItem(localStorage.key(i));
-					} catch(ignore) {}
-				}
+		var toRemove = [];
+		for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+			if(/^dsb\./.test(localStorage.key(i))) {
+				toRemove.push(localStorage.key(i));
 			}
-		}, 500);
+		}
+
+		toRemove.forEach(function(e) {
+			try {
+				localStorage.removeItem(e);
+			} catch(ignore) {}
+		});
 
 		$dripBot = null;
 	}
